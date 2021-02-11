72594fea79412cfaba492e7b0dc68dfcab3451f4
Matthew Cook <matthewcook13987@gmail.com>
2021-02-11 00:25:46 +0000

    Added a request function which takes the users input and uses that as the URL for it's request.

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,16 @@
 const request = require('request');
+const fs = require('fs')
+
+
 
 let input = process.argv.splice(2);
+let URL = input[0];
+
+
+request(URL, function(error,response,body){
+  console.log(`Error: ${error}`)
+  console.log(`Status Code: ${response}`)
+  console.log(`Body: ${body}`)
+})
 
 console.log(input);
